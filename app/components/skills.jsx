dd6c325f3bf0d4deefb46d1688a330f6a38dd837
Cory Goad <thebrickgoat@gmail.com>
2023-11-13 22:45:00 -0500

    pips/animations

--- app/components/skills.jsx
+++ app/components/skills.jsx
@@ -6,7 +6,7 @@ import Skill from './skill';
 
 export default function Skills() {
     const skills = [
-        { title: 'HTML/CSS/JS', description: '10 Years of experience building frontends with HTML, CSS, and JS', image: '/skills/html.svg' },
+        { title: 'HTML/CSS/JS', description: '10 Years of experience building frontends with HTML, CSS, and JS', image: '/skills/html.svg', pips: [{ title: 'Exepertise', filled: 4 }] },
         { title: 'Node.js', description: '5 Years in Node.js trying geussing what version the person that built this was using', image: '/skills/node.svg' },
         { title: 'React', description: '3 Years in React not understanding how refs actually work', image: '/skills/react.svg' },
         { title: 'Shopify', description: '6 Years in Shopify complaining about the multipage page checkout', image: '/skills/shopify.svg' },
@@ -91,15 +91,33 @@ export default function Skills() {
             </div>
             <div className="md:flex items-center gap-16 pt-8">
                 <div className="flex flex-col w-full md:w-1/3">
-                    <Image src={selectedSkill.image} alt={selectedSkill.title} width={150} height={100} className="object-cover mx-auto" />
+                    <div className="relative min-h-[250px] mx-auto">
+                        <Image src={selectedSkill.image.split('.', 1) + '.png'} alt={selectedSkill.title} width={150} height={100} className="bounce relative object-cover  z-20" />
+                        <Image src="/skills/arm.gif" alt={selectedSkill.title} width={150} height={100} className="absolute left-10 top-4 z-20" />
+                        <Image src="/skills/arm.gif" alt={selectedSkill.title} width={150} height={100} className="absolute -left-12 top-4 z-10" />
+                        <Image src="/skills/leg.gif" alt={selectedSkill.title} width={150} height={100} className="bounce absolute -left-4 top-24 z-10" />
+                        <Image src="/skills/leg.gif" alt={selectedSkill.title} width={150} height={100} className="bounce absolute left-8 top-24 z-10" />
+                        <div className='skillShadow'></div>
+                    </div>
+
                     <div className='mt-4'>
                         <h3 className="pb-4 font-bold text-4xl">{selectedSkill.title}</h3>
                         <h3 className="pb-4 text-xl">{selectedSkill.description}</h3>
+                        {selectedSkill.pips?.map((pip, index) => (
+                            <div className="flex space-x-2 mt-4 items-center">
+                                <div>{pip.title}</div>
+                                {
+                                    Array(5).fill(0).map((_, i) => (
+                                        <div className={`w-3 border-white border-2 h-3 ${pip.filled < i + 1 ? "" : "bg-white"} rounded-full`}></div>
+                                    ))
+                                }
+                            </div>
+                        ))}
                     </div>
                 </div>
                 <div className=" flex-col w-full md:w-2/3">
                     <h2 className="pb-8 text-4xl">
-                    <Image src='/skills/choose.png' alt={selectedSkill.title} width={675} height={36} className="object-cover m-auto w-3/4" />
+                        <Image src='/skills/choose.png' alt={selectedSkill.title} width={675} height={36} className="object-cover m-auto w-3/4" />
 
                     </h2>
 
