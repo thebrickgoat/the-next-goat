f2155b5a93592ecd5df89d6f26182299a22362de
Cory Goad <thebrickgoat@gmail.com>
2023-11-23 12:13:00 -0500

    removed double click

--- app/components/movableIcon.jsx
+++ app/components/movableIcon.jsx
@@ -1,23 +1,18 @@
-import React from "react";
+import { useRef } from "react";
 
 import Draggable from "react-draggable";
 import Image from "next/image";
 
 export default function Movable(props) {
-  const nodeRef = React.useRef(null);
-  const [lastClickTime, setLastClickTime] = React.useState(0);
+  const nodeRef = useRef(null);
 
   const handleClick = () => {
-    const now = Date.now();
-    if (now - lastClickTime < 500) {
-      if (props.openPopup) {
-        props.openPopup();
-      } else {
-        const element = document.getElementById(props.link);
-        element?.scrollIntoView({ behavior: "smooth" });
-      }
+    if (props.openPopup) {
+      props.openPopup();
+    } else {
+      const element = document.getElementById(props.link);
+      element?.scrollIntoView({ behavior: "smooth" });
     }
-    setLastClickTime(now);
   };
 
   return (
